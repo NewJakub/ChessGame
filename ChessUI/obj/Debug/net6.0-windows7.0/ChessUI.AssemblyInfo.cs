40aabd5dbc6fc4dd20a56736d2057a0c9df9ca9e
Jakub Cernosek <jakub.cernosek1@gmail.com>
2024-04-12 18:49:41 +0200

    UI for settings menu.

--- ChessUI/obj/Debug/net6.0-windows7.0/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net6.0-windows7.0/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b2e9b91a78c137688143a8af1c1ebb464bf026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314b9d0cd5454ecfe853365dfdea8a80c83c56d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
