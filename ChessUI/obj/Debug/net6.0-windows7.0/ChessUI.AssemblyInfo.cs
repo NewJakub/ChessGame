afec32148194a9b0d7ab6ec30e6ef8af6543fb16
Jakub Cernosek <jakub.cernosek1@gmail.com>
2024-04-22 21:01:32 +0200

    Reworking on structure and logic

--- ChessUI/obj/Debug/net6.0-windows7.0/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net6.0-windows7.0/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4683d920179f6597ad51bbda2aacff317f181dcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94aaf0e69808317feac6f3e99b8b631fb6d23c97")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
