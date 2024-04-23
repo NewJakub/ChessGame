e90942a111c208329ca76f0d9ebe863f17a2a02f
Jakub Cernosek <jakub.cernosek1@gmail.com>
2024-04-23 12:57:56 +0200

    Updated routing

--- ChessUI/obj/Debug/net6.0-windows7.0/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net6.0-windows7.0/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afec32148194a9b0d7ab6ec30e6ef8af6543fb16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3778e94c9934922ffad800d666bdbbe354a811")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
